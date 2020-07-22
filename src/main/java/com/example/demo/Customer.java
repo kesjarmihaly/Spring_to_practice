678a4cc8d3004992f57ba491096627fc0e73cca1
mkesjar <kesjarmihaly@gmail.hu>
2020-07-22 19:02:17 +0200

    first commit

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class Customer {
 
+    @Id
     private Long id;
     private String firstName;
     private String lastName;
